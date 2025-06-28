e641d6c8218c46d093131a0c84c72e559576a1ba
professorandersonsilva <professor.anderson.a.silva@gmail.com>
2025-06-28 12:08:01 -0300

    Correção da função de login com o google

--- app.config.js
+++ app.config.js
@@ -36,7 +36,7 @@ export default {
     },
     ios: {
       bundleIdentifier: "com.ae.stoantoniobarbalhacliente",
-      buildNumber: "1.0.2",
+      buildNumber: "1.0.3",
       config: {
         googleMapsApiKey: "AIzaSyBY4ZBNVZ1VkyqJqY_M7u3LPdT6Ielcuw0"
       },
