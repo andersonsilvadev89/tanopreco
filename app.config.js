0d4a6b964bb6efdd1af197fd123d5ec4e0a7c37e
andersonsilvadev89 <andersonsilva.dev89@gmail.com>
2025-10-31 16:52:15 -0300

    feat: Adiciona google-services.json ao .gitignore para o preview

--- app.config.js
+++ app.config.js
@@ -27,7 +27,7 @@ export default {
           apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
         },
       },
-      versionCode: 14,
+      versionCode: 15,
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff"
