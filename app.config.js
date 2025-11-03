a202ff212c14bf855306480dda9953f45afb0a0a
andersonsilvadev89 <andersonsilva.dev89@gmail.com>
2025-11-03 13:51:51 -0300

    Criando a variavel de ambiente para IOS

--- app.config.js
+++ app.config.js
@@ -60,7 +60,7 @@ export default {
       config: {
         googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
       },
-      googleServicesFile: process.env.GOOGLE_SERVICES_FILE_IOS,
+      googleServicesFile: process.env.GOOGLE_SERVICES_PLIST,
       infoPlist: {
         "ITSAppUsesNonExemptEncryption": false,
         NSPhotoLibraryUsageDescription: "Precisamos de acesso à sua galeria de fotos.",
